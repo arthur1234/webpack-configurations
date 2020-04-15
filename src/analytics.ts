f444c98007b4e203134d81eb6ca18f4581ad6a25
arthur12324 <mirumind@gmail.com>
2020-04-15 22:25:25 +0300

    Initial commit

---
+++ src/analytics.ts
@@ -0,0 +1,32 @@
+
+import * as $ from "jquery";
+
+function createAnalytics(): object {
+    let counter = 0;
+    let isDestroyed: boolean = false;
+
+    const listener = ():number => counter++;
+
+    // document.addEventListener('click', listener);
+    $(document).on('click', listener);
+    console.log('test');
+
+    return {
+        destroy() {
+           // document.removeEventListener('click', listener);
+            $(document).off('click', listener);
+
+            isDestroyed = true;
+        },
+        getClicks() {
+
+            if (isDestroyed) {
+                return `Analytics is destroyed. Total clicks = ${counter}`;
+            }
+            return counter;
+        }
+    }
+
+}
+
+window['analytics'] = createAnalytics();
